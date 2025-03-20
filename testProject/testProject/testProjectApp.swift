1ece8fcc1cb5a782a0e4ab724bb87508deb85466
luissaravia002 <156035764+luissaravia002@users.noreply.github.com>
2025-03-19 23:16:27 -0400

    Add files via upload

---
+++ testProject/testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Luis Saravia on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
